25026c3a3daee17fd4eea494170fcef7af3a583e
AndresFGar <afgar.x@gmail.com>
2022-02-27 14:38:56 -0500

    Validate Password

--- app/src/main/java/com/example/login/SignIn.java
+++ app/src/main/java/com/example/login/SignIn.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,30 +25,64 @@ public class SignIn extends AppCompatActivity {
 
         final TextView messageEmail = (TextView)findViewById(R.id.messageEmail);
 
+        final TextView messagePasword = (TextView)findViewById(R.id.messagePassword);
+
+        final EditText passwordValidate=(EditText)findViewById(R.id.password);
+
+        Button btnSignIn = (Button)findViewById(R.id.BtnSignIn);
+        btnSignIn.setEnabled(false);
 
         String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        String passwordPattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%!*^&+=])(.{8,})$";
 
         emailValidate .addTextChangedListener(new TextWatcher() {
             public void afterTextChanged(Editable s) {
 
                 if (s.toString().matches(emailPattern))
                 {
-                   // Toast.makeText(getApplicationContext(),"valid email address",Toast.LENGTH_SHORT).show();
-                    // or
-                    messageEmail.setText("valid email");
+                    messageEmail.setText("Valid email");
+                    btnSignIn.setEnabled(true);
                 }
                 else
                 {
                     //Toast.makeText(getApplicationContext(),"Invalid email address",Toast.LENGTH_SHORT).show();
                     //or
-                    messageEmail.setText("invalid email");
+                    messageEmail.setText("Invalid email");
+                    btnSignIn.setEnabled(false);
                 }
             }
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // other stuffs
             }
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // other stuffs
+            }
+        });
+
+
+        passwordValidate.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if (s.toString().matches(passwordPattern))
+                {
+                    messagePasword.setText("Valid Password");
+                    btnSignIn.setEnabled(true);
+                }
+                else
+                {
+                    messagePasword.setText("Invalid Password");
+                    btnSignIn.setEnabled(false);
+                }
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
             }
         });
 
