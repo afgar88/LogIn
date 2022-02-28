6b0eabad18f4d4d59c2e66f553fc7c28ea0048e4
AndresFGar <afgar.x@gmail.com>
2022-02-27 20:45:11 -0500

    All sign In validation works

--- app/src/main/java/com/example/login/SignIn.java
+++ app/src/main/java/com/example/login/SignIn.java
@@ -1,17 +1,28 @@
 package com.example.login;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class SignIn extends AppCompatActivity {
+
+    boolean email=false;
+    boolean password=false;
+    boolean confirmPas=false;
+     private Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +32,22 @@ public class SignIn extends AppCompatActivity {
         getSupportActionBar().setTitle("Sign In");
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
 
-        final EditText emailValidate = (EditText)findViewById(R.id.email);
+         EditText emailValidate = (EditText)findViewById(R.id.email);
+         TextView messageEmail = (TextView)findViewById(R.id.messageEmail);
+         TextView messagePasword = (TextView)findViewById(R.id.messagePassword);
+         EditText passwordValidate=(EditText)findViewById(R.id.password);
+         EditText confirmPasword=(EditText)findViewById(R.id.confirmpassword);
+         TextView messageConfirmPassword=(TextView)findViewById(R.id.messageConfirmPassword);
+         btnSignIn=(Button)findViewById(R.id.BtnSignIn);
 
-        final TextView messageEmail = (TextView)findViewById(R.id.messageEmail);
 
-        final TextView messagePasword = (TextView)findViewById(R.id.messagePassword);
-
-        final EditText passwordValidate=(EditText)findViewById(R.id.password);
-
-        final EditText confirmPasword=(EditText)findViewById(R.id.confirmpassword);
-
-        final TextView messageConfirmPassword=(TextView)findViewById(R.id.messageConfirmPassword);
-
-        Button btnSignIn = (Button)findViewById(R.id.BtnSignIn);
         btnSignIn.setEnabled(false);
+
 
         String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
         String passwordPattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%!*^&+=])(.{8,})$";
-        String Password="";
+
+
 
         emailValidate .addTextChangedListener(new TextWatcher() {
             public void afterTextChanged(Editable s) {
@@ -46,14 +55,15 @@ public class SignIn extends AppCompatActivity {
                 if (s.toString().matches(emailPattern))
                 {
                     messageEmail.setText("Valid email");
-                    btnSignIn.setEnabled(true);
+                    messageEmail.setTextColor(Color.parseColor("#10BC17"));
+                    email=true;
+                    validatefields();
                 }
                 else
                 {
-                    //Toast.makeText(getApplicationContext(),"Invalid email address",Toast.LENGTH_SHORT).show();
-                    //or
-                    messageEmail.setText("Invalid email");
-                    btnSignIn.setEnabled(false);
+                    messageEmail.setText("Invalid Email");
+                    emailValidate.setError("example@xxx.com");
+                    messageEmail.setTextColor(Color.parseColor("#FF1503"));
                 }
             }
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -70,11 +80,14 @@ public class SignIn extends AppCompatActivity {
                 if (s.toString().matches(passwordPattern))
                 {
                     messagePasword.setText("Valid Password");
-                    btnSignIn.setEnabled(true);
+                    messagePasword.setTextColor(Color.parseColor("#10BC17"));
+                    password=true;
+                    validatefields();
                 }
                 else
                 {
                     messagePasword.setText("Invalid Password");
+                    passwordValidate.setError("Your password should have a minimum of 8 characters and contain at least one number, one uppercase and one lower case letter");
                     btnSignIn.setEnabled(false);
                 }
 
@@ -96,7 +109,10 @@ public class SignIn extends AppCompatActivity {
                 if (s.toString().equals(passwordValidate.getText().toString()))
                 {
                     messageConfirmPassword.setText("Mach");
-                    btnSignIn.setEnabled(true);
+                    messageConfirmPassword.setTextColor(Color.parseColor("#10BC17"));
+                    confirmPas=true;
+                    validatefields();
+
                 }
                 else
                 {
@@ -115,6 +131,20 @@ public class SignIn extends AppCompatActivity {
             }
         });
 
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Myintent = new Intent(getBaseContext(),Content.class);
+                startActivity(Myintent);
+            }
+        });
+
+
+    }
+    public void validatefields (){
+        if (email==true && password==true && confirmPas==true){
+            btnSignIn.setEnabled(true);
+        }
 
     }
 
